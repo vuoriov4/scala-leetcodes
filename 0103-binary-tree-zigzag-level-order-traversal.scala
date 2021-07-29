d99c17603b4e14e6427162376a1dfc20407d2c04
Valtteri Vuorio <vuoriov4@gmail.com>
2021-07-29 07:51:58 +0300

    Create 0103-binary-tree-zigzag-level-order-traversal.scala

---
+++ 0103-binary-tree-zigzag-level-order-traversal.scala
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+import collection.mutable.ArrayBuffer
+object Solution {
+    def zigzagLevelOrder(root: TreeNode): List[List[Int]] = {
+        var result = List[List[Int]]()
+        if (root == null) return result
+        val q = collection.mutable.Queue[(TreeNode, Int)]()
+        q.enqueue((root, 0))
+        while (q.nonEmpty) {
+            val (n, d) = q.dequeue()
+            if (result.length == d) result = List[Int]() :: result
+            if (d % 2 == 0) {
+                result = result.updated(0, n.value :: result(0))
+            } else {
+                result = result.updated(0, result(0) :+ n.value)
+            }
+            if (n.right != null) q.enqueue((n.right, d+1))
+            if (n.left != null) q.enqueue((n.left, d+1))
+        }
+        result.reverse
+    }
+}
