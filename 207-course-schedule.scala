bf74dcbd7e0dccd21500792ded93a034538ff3d0
Valtteri Vuorio <vuoriov4@gmail.com>
2021-08-23 08:15:34 +0300

    Create 207-course-schedule.scala

---
+++ 207-course-schedule.scala
@@ -0,0 +1,27 @@
+object Solution {
+    var nodes = Set[Int]()
+    var edges = Map[Int, List[Int]]()
+    var visited = Set[Int]()
+    def canFinish(numCourses: Int, prerequisites: Array[Array[Int]]): Boolean = {
+        prerequisites.foreach(t => {
+            nodes = nodes + t(0)
+            nodes = nodes + t(1)
+            val out = t(0) +: edges.getOrElse(t(1), List[Int]())
+            edges = edges + (t(1) -> out)
+        })
+        val res = !nodes.exists(x => bfs(x, List[Int](x)))
+        nodes = Set[Int]()
+        edges = Map[Int, List[Int]]()
+        visited = Set[Int]()
+        res
+    }
+    def bfs(x: Int, path: List[Int]): Boolean = {
+        visited = visited + x
+        val successors = edges.getOrElse(x, List[Int]())
+        successors.exists(y => {
+            if (path.contains(y)) true
+            else if (!visited.contains(y)) bfs(y, y +: path)
+            else false
+        })
+    }
+}
